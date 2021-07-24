e4de2c21afc5ac6af6cdcf97625c159e6210c0e3
mahesh <mail.mahesh.lk@gmail.com>
2021-07-24 09:34:34 +0800

    rest-controller

--- micronaut-welcome/src/main/kotlin/io/mahesh/HelloWorld.kt
+++ micronaut-welcome/src/main/kotlin/io/mahesh/HelloWorld.kt
@@ -1,4 +1,18 @@
 package io.mahesh
 
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+
+@Controller("/welcome")
 class HelloWorld {
+
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
+
+    @Get("/hello")
+    fun index(): String? {
+        return "Hello World"
+    }
 }
